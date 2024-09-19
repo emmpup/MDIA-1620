760f4b184eac5c40972dd29adb8255aec3282ee4
emily misola <emilymisola@my.bcit.ca>
2024-09-19 15:42:25 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,11 @@
+console.log("horses are great!")
+let horseName = "Mango"; // this is a string
+horseName = "Charlie"; // for strings, preferr double quotes
+let horseAge = 5;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = true;
+isHorseInStable = false
+console.log(isHorseInStable)
+horseName = "Cookie"
+console.log(horseName)
