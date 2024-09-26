4aec099317ee66b338f57a219912660f16143b39
emily misola <emilymisola@my.bcit.ca>
2024-09-26 15:51:16 -0700

    add lab 4 code in my stables file

--- stables.js
+++ stables.js
@@ -1,6 +1,6 @@
 console.log("horses are great!");
-let horseName = "Mango"; // this is a string
-horseName = "Charlie"; // for strings, preferr double quotes
+let horseName = "Charlie"; // this is a string
+horseName = "Mango"; // for strings, preferr double quotes
 let horseAge = 5;
 console.log(horseName);
 console.log(horseAge);
@@ -20,3 +20,11 @@ let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
 let stableIntroduction = `Welcome to "${horseName}'s" stable`;
 console.log(stableIntroduction)
 console.log(rentIntroduction)
+
+// Lab 4 Code //////
+
+let horseNickname = "Fruit";
+console.log(`The name of my horse is ${horseName} and its nickname is ${horseNickname}`);
+
+let stableDiscount = ((STABLE_MONTHLY_FEE * 3) * 0.1);
+console.log("You would save $" + stableDiscount + " with a 10% discount applied to a 3 month stay");
