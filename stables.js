a31f79c8d61dd459ac6581f3f1148ee617f6bd66
emily misola <emilymisola@my.bcit.ca>
2024-09-26 14:37:25 -0700

    add operators to my stable file

--- stables.js
+++ stables.js
@@ -1,11 +1,22 @@
-console.log("horses are great!")
+console.log("horses are great!");
 let horseName = "Mango"; // this is a string
 horseName = "Charlie"; // for strings, preferr double quotes
 let horseAge = 5;
 console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = true;
-isHorseInStable = false
-console.log(isHorseInStable)
-horseName = "Cookie"
-console.log(horseName)
+isHorseInStable = false;
+console.log(isHorseInStable);
+horseName = "Cookie";
+console.log(horseName);
+
+const STABLE_MONTHLY_FEE = 100;
+console.log((STABLE_MONTHLY_FEE + (0.5 * STABLE_MONTHLY_FEE)) / 2);
+
+let horseIntroduction = " is the name of my horse";
+// let rentIntroduction = "It costs $" + STABLE_MONTHLY_FEE + " to board " + horseName;
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
+// let stableIntroduction = "Welcome to " + horseName + "'s stable";
+let stableIntroduction = `Welcome to "${horseName}'s" stable`;
+console.log(stableIntroduction)
+console.log(rentIntroduction)
