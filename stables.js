a68eff8b428d90255d81425282ea2975e1832c65
emily misola <emilymisola@my.bcit.ca>
2024-10-04 14:18:29 -0700

    add lab-5 code from my stables file

--- stables.js
+++ stables.js
@@ -1,13 +1,11 @@
 console.log("horses are great!");
 let horseName = "Charlie"; // this is a string
 horseName = "Mango"; // for strings, preferr double quotes
-let horseAge = 5;
+let horseAge = 1;
 console.log(horseName);
 console.log(horseAge);
-let isHorseInStable = true;
-isHorseInStable = false;
+let isHorseInStable = false;
 console.log(isHorseInStable);
-horseName = "Cookie";
 console.log(horseName);
 
 const STABLE_MONTHLY_FEE = 100;
@@ -21,10 +19,25 @@ let stableIntroduction = `Welcome to "${horseName}'s" stable`;
 console.log(stableIntroduction)
 console.log(rentIntroduction)
 
-// Lab 4 Code //////
+// Lab 4 Code
 
 let horseNickname = "Fruit";
 console.log(`The name of my horse is ${horseName} and its nickname is ${horseNickname}`);
 
 let stableDiscount = ((STABLE_MONTHLY_FEE * 3) * 0.1);
 console.log("You would save $" + stableDiscount + " with a 10% discount applied to a 3 month stay");
+
+// Lesson-5 Code
+visitingHorseName = "Lettuce";
+if ((horseName === "Mango" && isHorseInStable) &&  (visitingHorseName === "Straberry" || visitingHorseName === "Lettuce")) {
+    console.log(`${horseName} has a visitor and needs to come outside to see ${visitingHorseName}`);
+} else if ((horseName === "Mango" && !isHorseInStable) &&  (visitingHorseName === "Straberry" || visitingHorseName === "Lettuce")){
+    console.log(horseName + " is outside with " + visitingHorseName);
+} else {
+    console.log(horseName + " needs to figure out what is going on");
+}
+if (isHorseInStable) {
+    console.log(`${horseName} is inside`)
+} else {
+    console.log(horseName + " is outside")
+}
