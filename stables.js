ad966401efcaf873d8533dcdfc269385c68e9d5c
emily misola <emilymisola@my.bcit.ca>
2024-10-30 18:37:36 -0700

    format stables.js code

--- stables.js
+++ stables.js
@@ -1,7 +1,6 @@
 const STABLE_MONTHLY_FEE = 100;
 
-
-let horseName = "Mango"; 
+let horseName = "Mango";
 let horseNickname = "Fruit";
 let horseAge = 1;
 let isHorseInStable = false;
@@ -9,21 +8,21 @@ let isHorseInStable = false;
 let horseNames = [horseName, "Strawberry", "Banana"];
 let horseNicknames = [horseNickname, "Pie", "Peel"];
 let horseAges = [horseAge, 2, 10];
-let areHorsesInside = [isHorseInStable, false, true];
 
+let areHorsesInside = [isHorseInStable, false, true];
 let stableIntroduction = `Welcome to "${horseName}'s" stable`;
 let horseIntro = `There are ${horseNames.length} horses staying at my stables: ${horseNames[0]}, ${horseNames[1]}, and ${horseNames[2]}!`;
 
 function getHorseLocation(horse) {
-    if(areHorsesInside[horse]) {
-        return horseNames[horse] + " is inside.";
-    }
+  if (areHorsesInside[horse]) {
+    return horseNames[horse] + " is inside.";
+  }
 
-    return horseNames[horse] + " is outside.";
+  return horseNames[horse] + " is outside.";
 }
 
 function getHorseNicknames(horse, friend) {
-    const string = `${horseNames[horse]}'s nickname is ${horseNicknames[horse]}, and they have a friend named ${horseNames[friend]}, who is nicknamed ${horseNicknames[friend]}!`;
-    
-    return string;
+  const string = `${horseNames[horse]}'s nickname is ${horseNicknames[horse]}, and they have a friend named ${horseNames[friend]}, who is nicknamed ${horseNicknames[friend]}!`;
+
+  return string;
 }
