cfc816c7a916e3104720372874a66764f4ab75bd
emily misola <emilymisola@my.bcit.ca>
2024-12-04 16:40:07 -0800

    add final stables code to stables.js

--- stables.js
+++ stables.js
@@ -1,76 +1,334 @@
-console.log("horses are great!");
-let horseName = "Charlie"; // this is a string
-horseName = "Mango"; // for strings, preferr double quotes
-let horseAge = 1;
-console.log(horseName);
-console.log(horseAge);
-let isHorseInStable = false;
-console.log(isHorseInStable);
-console.log(horseName);
+// Welcome!
+//
+//                        ,, ,,, ,,,, ,,,,,,,,
+//           /\        /;; ;;;;;;;;;;;;;; ;;;/ ,;`.   ,,,,
+//          ;  `-.    /// //////// /////  // ,','`;. ///;;;;,.
+//         ,'   ,,`-.;;;;;; ;;;;;;; ;;;;// ,' ,'  `.`. ///;;//;,
+//        ,'   ;;;//////// ////// ///////,'  ,'     ; : ;;// ;//,
+//        `.  ;`;;;;;;;: ;;;;:;; ;:;:;;:;:  ,'     ,' : ;;;;;;;;/,
+//         `. `; :!::::!;;;;;!::::!;!;;!;:  `.   ,'  ,'///!!;;;;;;
+//           `._!!;!!!!;!!!!!;!!!!;!;!!;!!`.  `;'  ,'-.!!!//;;;////
+//              ;   .   .               ,        ,'    ::-!_///;;;;
+//            .' ,%'  ,%'     `%.   `%.;;   `%.   ;;   ,::  `! ////
+//           .', '    '    `%,  `:.   `::.   ::  :;    %::    `! ;;
+//          ,';;        `%, `;;.         `::. `.;;;    `:%   %:///
+//         ,';;'  ;      ;;  `::;   `%,    ;%:.  ::     ::     %`!/
+//       ,' ;.'  .%.    ;;    `;;     ;;   ' `;   %     ::    %  :
+//       :  `;;  %%%    `::   ;;     ;;;      `    `    ::     % `
+//       ;    ' .%%'  `%  ;   '  ,., `;;         `%,   ::'   %::%
+//      ;`. `.  %%%%   ;;   .___;;;;  '     `:    `;   ::     :::
+//      : :  ;  %%%%   ;: ,:' _ `.`.        ;;;   ;;   `::    :::.
+//      `.;  ;  `%%'  ;;' :: (0) ; :       ::'    ;      ::   `:::
+//       ,' ;'  %%'  ;;'  ;;.___,',;       ;;    ;;       ;   ,:::
+//      ,  ;'  :%:   ;;  ,'------''      ;;;'  .;;            :::'
+//     ,' ;;   ;%;   ;;  '             ::'    ,;;;            :::
+//     :  :'   :%:   `;             ;;;;'      ;;             ::%
+//     :  ;;   :%'   ;;   ;...,,;;''         ;;'    ;     ;   :::
+//     ;  `;   ::   ;;' ,:::'     .        .;;     ,'    ;;   `;;
+//     ;  ;'   :: .;;' ,:::'   ,::%.      ;;;    ,'     ;;    ,;;
+//     : ;;.  .:' ;;' ,:::' ;;:::' ;;    ;;'    ,'    ;;;    ;;;'
+//      :`;;  ::  ;;  ;;;' '  .    ;;    '  _,-'     ;;;     `;'
+//      : ;' .:'  ;; .::: ,%'`;   ;;;   _,-'       .;;;'     ;'
+//     ,' ;; ;;  ;;' :::' ,, .;  ;;  _,' ;      ,;;;'     ,;;'
+//    .'~~~~~~~~~._ ,;' ,',' ;;  ',-'   ,'    ,';;       ;;;'   ;;;
+//  ,'             `-.,' .'  ;; ,'     ,' ;;;;;;'       ,;;    ;;;
+// .';           .    `.,   ;; ,'      ;              ,;;%    ;;;
+// : ..       _.';     ;   '_,'       .'       ,,,,,,,%;;'    `;;;
+// `.  .     (_.' .  ;'  ,-'          :  ,,,,,;;;;;;;;;'      .;;;
+//   `-._        ___,' ,'             :.."""""`````'        ,;;;;
+//       `------'____.'               :                   ..;;;;
+//          `---'                     `.               ..;;;;'
+//                                     :......:::::::::;;;;'
+//                                      :::::::::::::::;'      ,;;;
+//                                       ;                   ;;;;'
+//                                       ;                 .;;;;
+//                                     ,'...:::::.        ;;;'
+//                                    .'          `;;;;;;''
+//                                    ;
+//                                    `----------------------------
 
-const STABLE_MONTHLY_FEE = 100;
-console.log((STABLE_MONTHLY_FEE + (0.5 * STABLE_MONTHLY_FEE)) / 2);
+//------------------------- Seting up shop -------------------------//
 
-let horseIntroduction = " is the name of my horse";
-// let rentIntroduction = "It costs $" + STABLE_MONTHLY_FEE + " to board " + horseName;
-let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
-// let stableIntroduction = "Welcome to " + horseName + "'s stable";
-let stableIntroduction = `Welcome to "${horseName}'s" stable`;
-console.log(stableIntroduction)
-console.log(rentIntroduction)
+let horses = [];
+let stableVisitorMessage =
+  "Welcome to the best stables in the world! I hope you enjoy your time!";
+let latePaymentFee = 25;
+let availableStalls = 10;
 
-// Lab 4 Code
+//------------------------- First day -------------------------//
 
-let horseNickname = "Fruit";
-console.log(`The name of my horse is ${horseName} and its nickname is ${horseNickname}`);
-
-let stableDiscount = ((STABLE_MONTHLY_FEE * 3) * 0.1);
-console.log("You would save $" + stableDiscount + " with a 10% discount applied to a 3 month stay");
-
-// Lesson-5 Code
-visitingHorseName = "Lettuce";
-if ((horseName === "Mango" && isHorseInStable) &&  (visitingHorseName === "Straberry" || visitingHorseName === "Lettuce")) {
-    console.log(`${horseName} has a visitor and needs to come outside to see ${visitingHorseName}`);
-} else if ((horseName === "Mango" && !isHorseInStable) &&  (visitingHorseName === "Straberry" || visitingHorseName === "Lettuce")){
-    console.log(horseName + " is outside with " + visitingHorseName);
-} else {
-    console.log(horseName + " needs to figure out what is going on");
+function Horse(
+  name,
+  nickname,
+  faveSnack,
+  monthlyRent,
+  isInside,
+  age,
+  color,
+  personality
+) {
+  this.name = name;
+  this.nickname = nickname;
+  this.faveSnack = faveSnack;
+  this.MONTHLY_RENT = monthlyRent;
+  this.isInside = isInside;
+  this.age = age;
+  this.color = color;
+  this.personality = personality;
+  this.introduction = function () {
+    console.log(
+      `This is ${this[name]}, their nickname is ${this.nickname} and love ${this.faveSnack}!`
+    );
+  };
+  this.rentNotice = function () {
+    console.log(
+      `${this.name}'s rent is $${this.MONTHLY_RENT}! If you don't pay in time, you will need to pay the late fee which is $${latePaymentFee}!`
+    );
+  };
+  this.swapHorsesInsideAndOutside = function () {
+    for (let i = 0; i < horses.length; i++) {
+      let horse = horses[i];
+      let horseName = horse.name;
+      let isInside = horse.isInside;
+      if (isInside) {
+        console.log(
+          horseName + " is being moved outside to spend time in the sun!"
+        );
+        horse.isInside = false;
+      } else {
+        console.log(
+          horseName +
+            " is being moved inside becasue they have spent too much time outside."
+        );
+        horse.isInside = true;
+      }
+    }
+  };
 }
-if (isHorseInStable) {
-    console.log(`${horseName} is inside`)
-} else {
-    console.log(horseName + " is outside")
+
+const mango = new Horse(
+  "Mango",
+  "Tango",
+  "chocolate",
+  100,
+  false,
+  1,
+  "orange",
+  "friendly"
+);
+
+const strawberry = new Horse(
+  "Strawberry",
+  "Shortcake",
+  "raspberries",
+  100,
+  false,
+  2,
+  "pink",
+  "easy-going"
+);
+
+const banana = new Horse(
+  "Banana",
+  "Peel",
+  "oranges",
+  100,
+  true,
+  10,
+  "yellow",
+  "fearful"
+);
+
+horses = [mango, strawberry, banana];
+
+const blueberry = new Horse(
+  "Blueberry",
+  "muffin",
+  "root beers",
+  100,
+  false,
+  5,
+  "blue",
+  "easy-going"
+);
+
+horses.push(blueberry);
+
+//I wanted a variety of horses being hungry or not
+//so all the horses that are in an even spot in the horses array will be hungry
+// and all the horses with an odd spot will be full
+for (let i = 0; i < horses.length; i++) {
+  let horse = horses[i];
+  if (i % 2 == 0) {
+    horse.isHungry = true;
+  } else {
+    horse.isHungry = false;
+  }
 }
 
-// Lesson-6 Code
-// Horse = ["name", age, isInside, "nickname"]
-let horseOne = [horseName, horseAge, isHorseInStable, horseNickname];
-let horseTwo = ["Strawberry", 2, false, "Pie"];
+//------------------------- Stable roster -------------------------//
 
-let horseNames = [horseName, "Strawberry", "Banana"];
-let horseAges = [horseAge, 2, 10];
-let horseNicknames = [horseNickname, "Pie", "Peel"];
-let areHorsesInside = [isHorseInStable, false, true];
-let checkHorsesInside = [];
-let horseIntro = `There are ${horseNames.length} horses staying at my stables: ${horseNames[0]}, ${horseNames[1]}, and ${horseNames[2]}!`;
-console.log(horseIntro);
+// horses = [
+//   {
+//     name: 'Mango',
+//     nickname: 'Tango',
+//     faveSnack: 'chocolate',
+//     MONTHLY_RENT: 100,
+//     isInside: false,
+//     age: 1,
+//     color: 'orange',
+//     personality: 'friendly',
+//     introduction: [Function (anonymous)],
+//     rentNotice: [Function (anonymous)],
+//     isHungry: true
+//   },
+//   {
+//     name: 'Strawberry',
+//     nickname: 'Shortcake',
+//     faveSnack: 'raspberries',
+//     MONTHLY_RENT: 100,
+//     isInside: false,
+//     age: 2,
+//     color: 'pink',
+//     personality: 'easy-going',
+//     introduction: [Function (anonymous)],
+//     rentNotice: [Function (anonymous)],
+//     isHungry: false
+//   },
+//   {
+//     name: 'Banana',
+//     nickname: 'Peel',
+//     faveSnack: 'oranges',
+//     MONTHLY_RENT: 100,
+//     isInside: true,
+//     age: 10,
+//     color: 'yellow',
+//     personality: 'fearful',
+//     introduction: [Function (anonymous)],
+//     rentNotice: [Function (anonymous)],
+//     isHungry: true
+//   },
+//   {
+//     name: 'Blueberry',
+//     nickname: 'muffin',
+//     faveSnack: 'root beers',
+//     MONTHLY_RENT: 100,
+//     isInside: false,
+//     age: 5,
+//     color: 'blue',
+//     personality: 'easy-going',
+//     introduction: [Function (anonymous)],
+//     rentNotice: [Function (anonymous)],
+//     isHungry: false
+//   }
+// ]
 
-if (areHorsesInside[0] == true) {
-    checkHorsesInside.push("inside");
-} else {
-    checkHorsesInside.push("outside");
+//---------------------- Growing business ----------------------//
+
+function updateAvailableStalls() {
+  stalls = availableStalls - horses.length;
+  if (stalls < 2) {
+    console.log("We need to build more stalls");
+  } else {
+    console.log("We have " + stalls + " available!");
+  }
 }
 
-if (areHorsesInside[1] == true) {
-    checkHorsesInside.push("inside");
-} else {
-    checkHorsesInside.push("outside");
+function lateRentCost(horse) {
+  horseName = horse.name;
+  rent = horse.MONTHLY_RENT;
+  lateRent = rent + latePaymentFee;
+  console.log(
+    `It will cost $${lateRent} since rent for ${horseName} is late. Don't be late again!`
+  );
 }
 
-if (areHorsesInside[2] == true) {
-    checkHorsesInside.push("inside");
-} else {
-    checkHorsesInside.push("outside");
+lateRentCost(horses[0]);
+
+function checkTreat(treat) {
+  for (let i = 0; i < horses.length; i++) {
+    let horse = horses[i];
+    let favorite = horse["faveSnack"];
+    if (favorite === treat) {
+      return console.log(`${horse.name} loves ${treat}!`);
+    } else {
+      console.log(
+        `${horse.name} does not like ${treat}, they like ${horse.faveSnack}`
+      );
+    }
+  }
 }
 
-console.log(`${horseNames[0]} is ${checkHorsesInside[0]}, ${horseNames[1]} is ${checkHorsesInside[1]}, and ${horseNames[2]} is ${checkHorsesInside[2]}`)
+function horseNickname(horse) {
+  return horse.nickname;
+}
+
+console.log(horseNickname(horses[0]));
+
+//------------------------- Day to day operations -------------------------//
+
+function rideableHorses() {
+  for (let i = 0; i < horses.length; i++) {
+    let horse = horses[i];
+    let horseName = horse["name"];
+    let personality = horse.personality;
+    let introduction = `${horseName} is ${personality}`;
+    if (personality === "friendly") {
+      console.log(
+        `${introduction} and is great with working with kids and new riders!`
+      );
+    } else if (personality === "easy-going") {
+      console.log(`${introduction} so they are a good riding horse!`);
+    } else {
+      console.log(`${introduction} so they aren't ready to ride with people`);
+    }
+  }
+}
+
+rideableHorses();
+
+function moveHorsesOutside(dark) {
+  for (let i = 0; i < horses.length; i++) {
+    let horse = horses[i];
+    let horseName = horse.name;
+    let isInside = horse.isInside;
+    if (dark === "dark") {
+      if (!isInside) {
+        console.log(`${horseName} is coming inside because it's getting dark!`);
+        isInside = true;
+      }
+    } else {
+      if (isInside) {
+        console.log(
+          horseName + " is being moved outside to spend time in the sun!"
+        );
+        horse.isInside = false;
+      } else {
+        console.log(horseName + " is already outside!");
+      }
+    }
+  }
+}
+
+moveHorsesOutside();
+
+function feedHorses() {
+  for (let i = 0; i < horses.length; i++) {
+    let horse = horses[i];
+    let horseName = horse.name;
+    let isInside = horse.isInside;
+    let faveSnack = horse.faveSnack;
+    if (isInside) {
+      console.log(`${horseName} is inside already having ${faveSnack}!`);
+      horse.isHungry = false;
+    } else {
+      console.log(`${horseName} is coming inside to for some ${faveSnack}!`);
+      horse.isHungry = false;
+      horse.isInside = true;
+    }
+  }
+}
+
+feedHorses();
+
+moveHorsesOutside("dark");
